363438f76f5d0c0fe2de4b76c8230e30023d462e
opeyemibami <bamigbadeopeyemi@gmail.com>
2020-12-09 21:47:50 +0100

    use object destructuring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=5ef9867c49d8698de6b1d140082d360d&query='+latitude+','+longitude+'&units=f'
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            const current = response.body.current
+            const current = body.current
             callback(undefined, {
                 temperature: current.temperature,
                 feelslike: current.feelslike,   
