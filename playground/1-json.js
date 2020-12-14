97ba6870e0c688bf84047a7335b6e4226b9a861d
opeyemibami <bamigbadeopeyemi@gmail.com>
2020-12-14 16:32:33 +0100

    Weather app code refactoring

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,18 +1,11 @@
 const fs = require('fs')
-const { connected } = require('process')
 
-// const book ={
-//     title: "Ego is the enemy",
-//     author: "Ryan Holiday"
-// }
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
 
-// const bookJSON = JSON.stringify(book)
-// fs.writeFileSync('1-json.json',bookJSON)
+user.name = 'Gunther'
+user.age = 54
 
-const jsonData = fs.readFileSync('1-json.json').toString()
-const parsedData = JSON.parse(jsonData)
-parsedData.name = 'Yhemmy'
-
-fs.writeFileSync('1-json.json',JSON.stringify(parsedData))
-
-console.log(parsedData)
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
