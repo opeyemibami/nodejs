6ebd860ec50e0529cadab558d94c48f09e1ff4f1
opeyemibami <bamigbadeopeyemi@gmail.com>
2020-12-14 11:49:07 +0100

    browse http request on clientside using fetch

--- web-server/public/js/index.js
+++ web-server/public/js/index.js
@@ -1,1 +1,11 @@
 console.log('Server side js on')
+
+fetch('http://localhost:3000/weather?address=Nigeria').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data)
+        }
+    })
+})
